69bc9583e0bd0f604e9050cd8dbfab9ae38f1dea
siyidongfang <csi@atmos.ucla.edu>
2022-04-28 21:27:47 -0700

    update

--- newexp_uc/createRuns.m
+++ newexp_uc/createRuns.m
@@ -6,11 +6,10 @@
 batch_name = 'exps_test'; 
 
 
-
 %%% Input parameters
 Ua = -1;      %%% Reference value -2
 Va = 0.5;       %%% Reference value 1
-Atide = 0.02; %%% Reference value 0.02 (based on Jourdain et al. 2019)
+Atide = 0; %%% Reference value 0.02 (based on Jourdain et al. 2019)
 Hi0 =0;       %%% Reference value 1
 Ai0 =0;       %%% Reference value 1
 m1km = 1000;
@@ -29,7 +28,7 @@ useSEAICE = false;
 exp_name = createRunName (Ua,Va,Atide,Hi0,Ai0,Ws,is_hires);
 
 %%% Create simulations
-exp_name = ['summer_new_res3km_' exp_name '_periodic']
+exp_name = ['res2km_' exp_name '_fresher0.5psu']
 
 newexp(batch_name,exp_name,Ua,Va,Atide,Hi0,Ai0,Ws,is_ContinuedRun,is_hires,useSEAICE);
 
